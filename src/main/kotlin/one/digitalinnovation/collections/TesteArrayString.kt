421a32ac3e5e65d8b6757c4eb582e99b59c7351f
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2021-04-01 13:40:13 -0300

    Added TesteArrayString.kt

--- src/main/kotlin/one/digitalinnovation/collections/TesteArrayString.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteArrayString.kt
@@ -1,6 +1,13 @@
 package one.digitalinnovation.collections
 
 fun main() {
-    val nomes = Array <String>(0) {""}
+    val nomes = Array(3) {""}
 
+    nomes[0] = "Mauro"
+    nomes[1] = "Sérgio"
+    nomes[2] = "Nilton"
+
+    for (nome in nomes){
+        println(nome)
+    }
 }
