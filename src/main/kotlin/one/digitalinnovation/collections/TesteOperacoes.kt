55e837fa4fb058e7305575285a0f2254c2e0d1d6
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2021-04-01 13:52:39 -0300

    Added TesteOperacoes.kt to learn about operations min, max, average and filter.

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
@@ -0,0 +1,17 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val salarios2 = doubleArrayOf(11000.00, 2500.15, 5000.11)
+    salarios2.sort()
+    salarios2.forEach { println(it) }
+
+    println("---------------------------")
+    println("Menor salário: " + salarios2.min())
+    println("Maior salário: " + salarios2.max())
+    println("Média salarial: " + salarios2.average())
+
+    val salariosMaiorQue2000 = salarios2.filter { it > 2600 }
+
+    println("---------------------------")
+    salariosMaiorQue2000.forEach { println(it) }
+}
