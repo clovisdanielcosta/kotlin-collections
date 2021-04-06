f1f23adf7f1498c72880846e8c529d4275aad7db
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2021-04-06 13:23:40 -0300

    Created TesteMutavelMap.kt to apply the concepts of mutable collections with repository an generic args. Create functions create and findById.

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteMutavelMap.kt
@@ -0,0 +1,15 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val joao = Funcionario("João", 1000.00, tipoContratacao = "CLT")
+    val maria = Funcionario("Maria", 3200.15, tipoContratacao = "PJ")
+    val pedro = Funcionario("Pedro", 1850.27, tipoContratacao = "CLT")
+
+    val repositorio = Repositorio<Funcionario>()
+
+    repositorio.create(joao.nome, joao)
+    repositorio.create(pedro.nome, pedro)
+    repositorio.create(maria.nome, maria)+
+
+    println(repositorio.findById(joao.nome))
+}
